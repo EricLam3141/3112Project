d333dd5920637a28d6602bbb110af31a4e829a22
Eric Lam <ericlam3.141@gmail.com>
2024-02-19 18:57:58 -0500

    Updated Project

--- InventoryManager.java
+++ InventoryManager.java
@@ -1,14 +1,36 @@
-
+import java.util.Scanner;
 public class InventoryManager {
     public static void main (String args[]) {
+        int off = 1;
+        Scanner inputScan = new Scanner(System.in);
         inventoryItems newItem = new inventoryItems();
         newItem.itemName = "5600x";
         System.out.println(newItem.getItemName());
-        newItem.setItem(0, "Ryzen 5600x", 199, 1 );
+        newItem.setItem(0, "Random item", 199, 1 );
         System.out.println(newItem.getPrice());
         System.out.println("Hello World");
         System.out.println("What");
 
-    }
+        int select = inputScan.nextInt();
+        while(off == 1 ) {
+            switch (select) {
+                
+                case 1:
+                    System.out.println("lol");
+                    break;
+                case 2:
+                    System.out.println("lol2");
+                    break;
+                case 3: 
+                    System.out.println("Out");
+                    off = 0;
+                    break;
+                }
+                select = inputScan.nextInt();
+                
+            }
+        }
+
 }
+
  
